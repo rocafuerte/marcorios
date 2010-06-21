2382029bb7cde611689c2179e26e7e1480b11d6d
Felix <felix@109-124-131-57.customer.t3.se>
2010-06-21 17:56:38 +0200

    ...

---
+++ homepage/admin.py
@@ -0,0 +1,8 @@
+from marcorios.homepage.models import *
+from django.contrib import admin
+
+admin.site.register(TextImage)
+admin.site.register(Place)
+admin.site.register(ShowAt)
+
+ 
--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class ShowStyle(models.Model):
+    name = models.CharField(max_length=100)
+
+# Administrator adds these when he sets up the system 
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.name    
+    # Define visible styles here instead of at the specific style
+    #visible = models.ManyToManyField(TextImage)
+
+# This could be used for: Contact, History, ...
+class TextImage(ShowStyle):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    # This in wrong    
+    showAt = models.ManyToManyField(Place,through='ShowAt') # This has to be restricted
+
+class ShowAt(models.Model):
+    show_style = models.ForeignKey(TextImage)  # ShowStyle
+    place = models.ForeignKey(Place)
+    order_no = models.IntegerField()
+    #language = 
+    
